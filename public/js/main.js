a4b402446f779ccd7708b168ba6bb119a8c05ac6
makobasuri <makobasuri@gmail.com>
2018-06-14 23:21:14 +0200

    added spritesheet class

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,59 @@
+import {loadTileset} from './loaders.js';
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-function loadImage(url) {
-	return new Promise(resolve => {
-		const image = new Image();
-		image.addEventListener('load', () => {
-			resolve(image)
-		});
-		image.src = url;
-	});
+function createBuffer(width = 32, height = 32) {
+	const newBuffer = document.createElement('canvas');
+	newBuffer.width = width;
+	newBuffer.height = height;
+	newBuffer.getContext('2d');
+	return newBuffer;
 }
 
-loadImage('/tiles/DungeonCrawl_ProjectUtumnoTileset.png')
-	.then(image => ctx.drawImage(
-		image,
-		704, 416, 32, 32,
-		0, 0, 32, 32
-	))
+class SpriteSheet {
+	constructor(image, width = 32, height = 32) {
+		this.image = image;
+		this.width = width;
+		this.height = height;
+		this.tiles = new Map();
+	}
+
+	define(name, x, y) {
+		const buffer = createBuffer();
+		// throws type error
+		buffer.drawImage(
+			this.image,
+			x * this.width,
+			y * this.height,
+			this.width,
+			this.height,
+			0,
+			0,
+			this.width,
+			this.height
+		);
+		this.tiles.set(name, buffer);
+
+		return this
+	}
+
+	draw(name, context, x, y) {
+		context.drawImage(
+			this.tiles.get(name),
+			x,
+			y
+		)
+	}
+}
+
+loadTileset('/tiles/DungeonCrawl_ProjectUtumnoTileset.png')
+	.then(image => {
+		const sprites = new SpriteSheet(image);
+		sprites.define('ground', 704, 416)
+		sprites.draw('ground', ctx, 45, 45)
+		ctx.drawImage(
+			image,
+			704, 416, 32, 32,
+			0, 0, 32, 32
+		)
+	})
